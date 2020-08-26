e077706d81a1022b142dcef893cfda862e9f2775
oliver fu <ofu997@gmail.com>
2020-08-26 15:09:11 -0700

    add project

---
+++ fetch-post.js
@@ -0,0 +1,41 @@
+document.getElementById('fetchForm').addEventListener('submit', submitPost);
+
+async function submitPost(e) {
+    e.preventDefault();
+    
+    let title = document.getElementById('titleInput').value;
+    let body = document.getElementById('bodyInput').value;
+    
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({title: title, body: body}),
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    };
+    
+    const postPromise = await fetch(
+        'https://jsonplaceholder.typicode.com/posts',
+        options
+    );
+    // error handling 
+    // if(postPromise.ok) {
+    //     const post = await postPromise.json();
+
+    //     title = post.title;
+    //     body = post.body;
+    // } else {
+    //     title = 'Error';
+    //     body = `Status: ${postPromise.status}`;
+    // }
+    
+    const post = await postPromise.json();
+    
+    title = post.title;
+    body = post.body;
+    
+    document.querySelector('.card-title').innerHTML = title;
+    document.querySelector('.card-text').innerHTML = body;
+    
+    document.getElementById('fetchForm').reset();
+}
---
+++ index.js
@@ -0,0 +1,84 @@
+fetch('people.json')
+	.then( response => {throw 3.14159} )
+	.then( json => 
+		json.forEach((person) => {
+            const div = document.createElement("div");
+            div.innerHTML = `${person.name} is ${person.age}`;
+            document.body.appendChild(div);
+        })
+    )
+    .catch(err => console.error(err));
+    
+fetch('people.json')
+.then( response => response.json() )
+.then( json => 
+    json.forEach((person) => {
+        const div = document.createElement("div");
+        div.innerHTML = `${person.name} is ${person.age}`;
+        document.body.appendChild(div);
+    })
+)
+.catch(err => console.error(err));
+    
+const getData = async () => {
+    const response = await fetch('people.json');
+    const data = await response.json();
+        
+    data.forEach( person => {
+        const div = document.createElement('div');
+        div.innerHTML = person.name;
+        document.body.appendChild(div);
+    })
+}
+
+getData();
+
+async function getPosts() {
+    const postsPromise = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await postsPromise.json();
+    let html = "";
+    
+    posts.slice(0, 5).forEach( post => {
+        const title = post.title;
+        const body = post.body;
+        html += `
+            <div class='post'>
+                <h3>${title}</h3>
+                <p>${body}</p>
+            </div>
+        `;
+    });
+    document.body.innerHTML = html;
+    console.log(`${posts.length}`)
+}
+
+getPosts();
+
+// error handling
+// async function getPosts() {
+//     const postsPromise = await fetch(
+//         "https://jsonplaceholder.typicode.com/posts"
+//     );
+    
+//     if(postsPromise.ok) {
+//         const posts = await postsPromise.json();
+//         let html = "";
+
+//         posts.slice(0, 5).forEach((post) => { 
+//             const title = post.title;
+//             const body = post.body;
+//             html += `
+//             <div class='post'>
+//                 <h3>${title}</h3>
+//                 <p>${body}</p>
+//             </div>
+//             `;
+//         });
+
+//         document.body.innerHTML = html;
+//     } else {
+//         console.error(`Error: ${postsPromise.status}`)
+//     }
+// }
+
+// getPosts();
