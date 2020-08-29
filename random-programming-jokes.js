452816ed7973c5d3db0ca1a72c5ec98a61d5d0db
oliver fu <ofu997@gmail.com>
2020-08-28 23:54:07 -0700

    start joke generator

---
+++ random-programming-jokes.js
@@ -0,0 +1,22 @@
+const setupDiv = document.getElementById("setup");
+const punchlineDiv = document.getElementById("punchline");
+const punchlintBtn = document.getElementById("punchlineBtn");
+const newJokeBtn = document.getElementById("newJokeBtn");
+let punchline; 
+
+// Setup an async function called getJoke
+// Create a variable called jokePromise that fetches a joke from https://official-joke-api.appspot.com/jokes/programming/random
+// create a variable called joke that consumes the json data
+
+const getJoke = async () => {
+    // const jokePromise = fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+    const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
+    const joke = await jokePromise.json();
+
+    setupDiv.innerHTML = joke[0].setup; 
+
+    // Create a global variable called punchline which will store the current punchline and will be updated with each new joke
+    // Assign the current jokes punchline to the punchline variable.
+}
+
+getJoke(); 
