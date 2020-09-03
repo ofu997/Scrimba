be04cc7025c0bc49e65314f885d5657dfb6ac48d
oliver fu <ofu997@gmail.com>
2020-09-02 20:58:22 -0700

    finish jokes

--- random-programming-jokes.js
+++ random-programming-jokes.js
@@ -1,22 +1,45 @@
 const setupDiv = document.getElementById("setup");
 const punchlineDiv = document.getElementById("punchline");
-const punchlintBtn = document.getElementById("punchlineBtn");
+const punchlineBtn = document.getElementById("punchlineBtn");
 const newJokeBtn = document.getElementById("newJokeBtn");
 let punchline; 
 
-// Setup an async function called getJoke
-// Create a variable called jokePromise that fetches a joke from https://official-joke-api.appspot.com/jokes/programming/random
-// create a variable called joke that consumes the json data
+// my attempt
+// const getPunchline = () => {
+//     punchlineDiv.innerHTML = punchline; 
+//     // Add the class "bubble" to the punchlineDiv
+//     // Toggle the "hidden" class on both buttons.        
+// }
 
-const getJoke = async () => {
+// punchlineBtn.onClick = getPunchline();
+
+punchlineBtn.addEventListener('click', getPunchline);
+// Add an event listener for the new joke button. When clicked it should call the getJoke function 
+newJokeBtn.addEventListener('click', getJoke);
+
+function getPunchline() {
+    punchlineDiv.innerHTML = punchline;
+    punchlineDiv.classList.add('bubble');
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
+}
+
+async function getJoke() {
     // const jokePromise = fetch('https://official-joke-api.appspot.com/jokes/programming/random');
     const jokePromise = await fetch('https://official-joke-api.appspot.com/jokes/programming/random');
     const joke = await jokePromise.json();
 
     setupDiv.innerHTML = joke[0].setup; 
 
-    // Create a global variable called punchline which will store the current punchline and will be updated with each new joke
-    // Assign the current jokes punchline to the punchline variable.
+    // Assign the current joke's punchline to the punchline variable.
+    punchline = joke[0].punchline;
+    // Clear the punchline div and remove the "bubble" class from the punchline div
+    punchlineDiv.innerHTML = '';
+    punchlineDiv.classList.remove('bubble')
+    punchlineBtn.classList.toggle('hidden');
+    newJokeBtn.classList.toggle('hidden');
 }
 
 getJoke(); 
+
+
